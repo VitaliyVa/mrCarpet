6301958e8a14f78d3baee684214ad38e674bcedd
VitaliyVa <vistet1428@gmail.com>
2023-05-11 21:24:41 +0300

    add html

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import './styles/global.scss'; // импорт глобальных стилей
-
-// импорт модулей для каждой страницы
-// import './pages/index/index';
-// import './pages/about/about';
-// import './pages/contact/contact';
