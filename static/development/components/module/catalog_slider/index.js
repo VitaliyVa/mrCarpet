3a385c2d3fd93bd19fb38070ed3355f4499d8d5b
ArtemGr98 <grishkoart98@gmail.com>
2023-05-15 15:43:40 +0300

    index

--- static/development/components/module/catalog_slider/index.js
+++ static/development/components/module/catalog_slider/index.js
@@ -1,1 +1,19 @@
 import './index.scss';
+import Swiper, {
+    Navigation,
+} from 'swiper';
+import 'swiper/swiper-bundle.css'
+import './index.scss'
+
+Swiper.use([Navigation])
+
+const catalog_slider = new Swiper(".catalog-swiper", {
+    slidesPerView: '4',
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
+
