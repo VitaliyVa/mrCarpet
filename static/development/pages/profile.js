02fc0ee72f6bba88a44f9afcd21f065a682dc195
Oleksandr Myronchuk <smironcuk35@gmail.com>
2023-09-29 22:37:41 +0300

    Order History Page

---
+++ static/development/pages/profile.js
@@ -0,0 +1,14 @@
+// script interface
+import "../components/common_componentc/reset/index";
+import "../components/common_componentc/normalize/index";
+import "../components/common_componentc/header/index";
+import "../components/common_componentc/footer/index";
+
+import "../components/interface/variables/index";
+import "../components/interface/input/index";
+import "../components/interface/size-label/index";
+
+import "../components/module/catalog_slider/index";
+import "../components/module/accordion/index";
+
+import "../components/pages/profile/index";
