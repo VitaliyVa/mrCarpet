047997f205b0568785af25eb5c37bacc6fd2baa2
Xanya <progamerr99@gmail.com>
2024-02-08 15:18:02 +0000

    static

---
+++ static_root/development/components/interface/button/index.js
@@ -0,0 +1,30 @@
+import './index.scss';
+ 
+// $('.btn-lean_more').on('mouseenter', function(){
+//     $(this).addClass('is-focus-over');
+//     $(this).removeClass('is-focus-out');
+// });
+// $('.btn-lean_more').on('mouseleave', function(){
+//     $(this).addClass('is-focus-out');
+//     $(this).removeClass('is-focus-over');
+
+// });
+
+    
+
+// [...$('.btn-black')].map(item=>{ 
+//     const current_width = $(item)[0].offsetWidth;
+//     console.log(current_width);
+//     $(item).css('width', `${current_width}px`);
+// });
+
+// [...$('.btn-yelow')].map(item=>{ 
+//     const current_width = $(item)[0].offsetWidth;
+//     console.log(current_width);
+//     $(item).css('width', `${current_width}px`);
+// });
+
+//  $('.burger').on('click',function(){
+//      $(this).toggleClass('active')
+//  })
+
---
+++ static_root/development/components/module/accordion/index.js
@@ -0,0 +1,7 @@
+import "./index.scss";
+
+document.querySelectorAll(".accordion__title").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.closest(".accordion_content__block").classList.toggle("active");
+  });
+});
