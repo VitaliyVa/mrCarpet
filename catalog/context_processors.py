80f2dd5723cb6a75eb7f84e82ace3ad39924e74b
Xanya <progamerr99@gmail.com>
2024-03-21 00:15:53 +0200

    favourite fix

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,9 +1,12 @@
-from .models import Favourite, ProductCategory, Product
+from .models import Favourite, ProductCategory, Product, FavouriteProducts
 from .utils import get_favourite
+
 
 def context(request):
     categories = ProductCategory.objects.all()
-    f_products = get_favourite(request).product.all()[::-1]
+    # f_products = get_favourite(request).product.all()[::-1]
+    raw_products = FavouriteProducts.objects.filter(favourite=get_favourite(request)).select_related('product')
+    f_products = [product.product for product in raw_products]
     products = Product.objects.all().order_by("-id")
     # f_products = favourite.product.all()[::-1]
     context = {
