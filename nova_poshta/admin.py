41eeca17cadfaf11ce193103ed5ece3b2649e44a
RHoryachko <rhoryachko@gmail.com>
2025-09-06 13:42:46 +0300

    ADD: Enhanced NovaPoshtaSettings admin interface by implementing custom permissions for adding, changing, and deleting settings; registered NovaPoshtaSettings with a dedicated admin class for improved management.

--- nova_poshta/admin.py
+++ nova_poshta/admin.py
@@ -5,4 +5,18 @@ from .models import Settlement, Warehouse, NovaPoshtaSettings
 # Register your models here.
 admin.site.register(Settlement)
 admin.site.register(Warehouse)
-admin.site.register(NovaPoshtaSettings)
+
+
+class NovaPoshtaSettingsAdmin(admin.ModelAdmin):
+    list_display = ['api_key']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def has_add_permission(self, request, obj=None):
+        return NovaPoshtaSettings.objects.count() == 0
+    
+    def has_change_permission(self, request, obj=None):
+        return True
+
+admin.site.register(NovaPoshtaSettings, NovaPoshtaSettingsAdmin)
